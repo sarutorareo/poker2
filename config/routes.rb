363897d3fa1efb66010dcdd33469a30371a52236
sarutorareo <sarutorareo@gmail.com>
2016-07-10 00:26:43 +0900

    ActiveCableを使ったチャットサンプルを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'rooms/show'
+
+  root to: 'rooms#show'
+  get 'room/show'
+
   get 'top/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "top#index"
+
+#  mount ActionCable.server => '/cable'
 end
