fdd555de503390b65c34f2c3ad1ac3717b22f555
sarutorareo <sarutorareo@gmail.com>
2016-09-04 00:36:42 +0900

    heroku 用 redisの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = ['https://hiyopoker.herokuapp.com', 'http://hiyopoker.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -83,4 +84,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # for heroku redis
+  config.web_socket_server_url = "wss://hiyopoker.herokuapp.com/cable"
 end
