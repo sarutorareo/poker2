165ee9eec7e9529d15953b7e1be3cdf927dc4d22
sarutorareo <sarutorareo@gmail.com>
2016-11-12 19:44:15 +0900

    productionのseedにもrobo_hiyotenを追加

--- db/seeds/production/users.rb
+++ db/seeds/production/users.rb
@@ -1,8 +1,9 @@
-@user_names = ['ichirou', 'jirou', 'hanako', 'hiyoten']
+@user_names = ['ichirou', 'jirou', 'hanako', 'hiyoten', 'robo_hiyoten']
 
 @user_names.each do |name|
   User.create(
     name: name,
-    chip: 150000
+    chip: 150000,
+    user_type: name.length >= 5 && name[0,5] == 'robo_' ? 1: 0
   )
 end
