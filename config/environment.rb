53d768aa251dae61a4cb0bf712b2009f6fe6ea1c
sarutorareo <sarutorareo@gmail.com>
2016-11-27 20:22:02 +0900

    ログに時刻を表示するようにした

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,13 @@
 # Load the Rails application.
 require_relative 'application'
 
+class Logger::MyFormatFormatter < Logger::Formatter
+  cattr_accessor(:datetime_format) { "%Y/%m/%d %H:%m:%S" }
+
+  def call(severity, timestamp, progname, msg)
+    "[#{timestamp.strftime(datetime_format)}] #{severity[0..2]}: #{String === msg ? msg : msg.inspect}\n"
+  end
+end
+
 # Initialize the Rails application.
 Rails.application.initialize!
