45c457d2a1a0fb3d606980968d5284b81691610e
sarutorareo <sarutorareo@gmail.com>
2016-12-20 00:08:16 +0900

    herokuで一部websocket通信が動作しない問題調査用デバッグメッセージ出力

--- app/jobs/hand_users_broadcast_job.rb
+++ app/jobs/hand_users_broadcast_job.rb
@@ -3,8 +3,11 @@ class HandUsersBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(room_id, hand_id)
-    ColorLog.clog "###### in HandUsersBroadcastJob perform room_id=#{room_id} hand_id=#{hand_id}"
+    # ColorLog.clog "###### in HandUsersBroadcastJob perform room_id=#{room_id} hand_id=#{hand_id}"
+    Message.create! content: "before HandUsers [#{hand_id}]", room_id: room_id, user_name: 'dealer'
+    Message.create! content: " HandUsers is [#{render_user_list(hand_id)}]", room_id: room_id, user_name: 'dealer'
     ActionCable.server.broadcast "poker_channel_#{room_id}", {type: "msg_hand_users", DOM_hand_user_list: render_user_list(hand_id)}
+    Message.create! content: "before HandUsers [#{hand_id}]", room_id: room_id, user_name: 'dealer'
   end
 
   private
