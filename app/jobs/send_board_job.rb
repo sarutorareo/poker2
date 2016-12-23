2e5612ec214786984e5bbf5961a5135b0d8a0e21
sarutorareo <sarutorareo@gmail.com>
2016-12-23 20:22:56 +0900

    試しにMessageをコメントアウト

--- app/jobs/send_board_job.rb
+++ app/jobs/send_board_job.rb
@@ -3,9 +3,9 @@ class SendBoardJob < ApplicationJob
 
   def perform(room_id, board_str)
     p "################## in SendBoardJob.perform"
-    Message.create! content: "before SendBoardJob [#{board_str}]", room_id: room_id, user_name: 'dealer'
+    # Message.create! content: "before SendBoardJob [#{board_str}]", room_id: room_id, user_name: 'dealer'
     ActionCable.server.broadcast "poker_channel_#{room_id}", {type: "msg_update_board", DOM_board: board_str}
-    Message.create! content: "after SendBoardJob [#{board_str}]", room_id: room_id, user_name: 'dealer'
+    # Message.create! content: "after SendBoardJob [#{board_str}]", room_id: room_id, user_name: 'dealer'
   end
 
   private
