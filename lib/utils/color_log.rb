db75aa5d1074f617239efe115ce7e454e289a30e
sarutorareo <sarutorareo@gmail.com>
2016-12-25 01:03:44 +0900

    試しにColorLog内のRails.logger.debugをコメントアウト

--- lib/utils/color_log.rb
+++ lib/utils/color_log.rb
@@ -5,6 +5,6 @@ module ColorLog
     if Rails.env == "test"
       p obj
     end
-    Rails.logger.debug "\e[44m#{obj.kind_of?(String) ? obj: obj.inspect}\e[0m"
+    #Rails.logger.debug "\e[44m#{obj.kind_of?(String) ? obj: obj.inspect}\e[0m"
   end
 end
