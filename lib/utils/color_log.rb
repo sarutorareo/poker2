30bdf377d6dd3711e583e4f5927078c222c9bd06
sarutorareo <sarutorareo@gmail.com>
2016-12-25 01:07:59 +0900

    試しにColorlog.clogをすべてコメントアウト

--- lib/utils/color_log.rb
+++ lib/utils/color_log.rb
@@ -1,10 +1,10 @@
 #ColorLog
 module ColorLog
   def self.clog (obj)
-    #CE.once.ch :black, :blue
-    if Rails.env == "test"
-      p obj
-    end
-    #Rails.logger.debug "\e[44m#{obj.kind_of?(String) ? obj: obj.inspect}\e[0m"
+#    #CE.once.ch :black, :blue
+#    if Rails.env == "test"
+#      p obj
+#    end
+#    #Rails.logger.debug "\e[44m#{obj.kind_of?(String) ? obj: obj.inspect}\e[0m"
   end
 end
