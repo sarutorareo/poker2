df1e4c765cdebdcf70b6e178379046b99e2d8ae6
sarutorareo <sarutorareo@gmail.com>
2016-12-25 10:09:28 +0900

    試しにColorLogをjobの中でrequire 'color_log'

--- app/jobs/hand_users_broadcast_job.rb
+++ app/jobs/hand_users_broadcast_job.rb
@@ -1,16 +1,12 @@
-#require 'color_log'
+require 'color_log'
 
 WAIT_TIME_HAND_USERS_BROAD_CAST_JOB = 0
 class HandUsersBroadcastJob < ApplicationJob
   queue_as :default
 
-
   def perform(room_id, hand_id)
    ColorLog.clog "###### in HandUsersBroadcastJob perform room_id=#{room_id} hand_id=#{hand_id}"
-  #  Message.create! content: "before HandUsers [#{hand_id}]", room_id: room_id, user_name: 'dealer'
-  #  Message.create! content: " HandUsers is [#{render_user_list(hand_id)}]", room_id: room_id, user_name: 'dealer'
     ActionCable.server.broadcast "poker_channel_#{room_id}", {type: "msg_hand_users", DOM_hand_user_list: render_user_list(hand_id)}
-  #  Message.create! content: "before HandUsers [#{hand_id}]", room_id: room_id, user_name: 'dealer'
   end
 
   private
