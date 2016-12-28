dc0bff3355788b918c29d0c997538bd77903843a
sarutorareo <sarutorareo@gmail.com>
2016-12-28 16:46:06 +0900

    CEによる色変更を、p の直前に行うように変更

--- lib/utils/color_log.rb
+++ lib/utils/color_log.rb
@@ -1,8 +1,8 @@
 #ColorLog
 module ColorLog
   def self.clog (obj)
-    CE.once.ch :black, :blue
     if Rails.env == "test"
+      CE.once.ch :black, :blue
       p obj
     end
     Rails.logger.debug "\e[44m#{obj.kind_of?(String) ? obj: obj.inspect}\e[0m"
