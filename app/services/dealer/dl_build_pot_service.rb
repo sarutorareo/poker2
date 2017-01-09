b4968654bb9f09172d17b494d49c2c7e3d4a6550
sarutorareo <sarutorareo@gmail.com>
2017-01-10 00:00:08 +0900

    サイドポットを実装

--- app/services/dealer/dl_build_pot_service.rb
+++ app/services/dealer/dl_build_pot_service.rb
@@ -4,23 +4,40 @@ class DlBuildPotService < DlHandServiceBase
   end
 
   def do!
+    pots = []
+    # pots = _makePots(pots, 0)
     hand = Hand.find(@hand_id)
-    pot = Pot.new
-    hand.hand_users.each do |hu|
-      pot.chip += hu.hand_total_chip
-      pot.hand_users << hu unless hu.last_action.fold?
-    end
-    [pot]
+    before_min_chip = 0
+
+    _make_pot(hand, before_min_chip, pots)
   end
 
-private
+  private
 
-  def get_min_hand_total_chip(hand)
+  def _get_min_hand_total_chip(hand, before_min_chip)
     min_chip = nil
     hand.hand_users.each do |hu|
+      next if hu.hand_total_chip <= before_min_chip
       min_chip = hu.hand_total_chip if min_chip.nil? || hu.hand_total_chip < min_chip
     end
     min_chip
   end
+
+  def _make_pot(hand, before_min_chip, pots)
+    min_chip = _get_min_hand_total_chip(hand, before_min_chip)
+    return pots if min_chip.nil?
+
+    pot = Pot.new
+    hand.hand_users.each do |hu|
+      next if hu.hand_total_chip <= before_min_chip
+      pot.chip += min_chip - before_min_chip
+      next if hu.last_action.fold?
+      pot.hand_users << hu
+    end
+    pots << pot
+
+    _make_pot(hand, min_chip, pots)
+  end
+
 end
 
--- spec/services/dealer/dl_build_pot_service_spec.rb
+++ spec/services/dealer/dl_build_pot_service_spec.rb
@@ -73,8 +73,57 @@ RSpec.describe DlBuildPotService, type: :service do
         expect(pot.hand_users.map{|p| p.user_id}.include?(@user_2.id)).to eq(false)
       end
     end
+    context 'user_1が200, user_2が100, user_3が101かけているがfold, user_4が200かけているとき' do
+      before do
+        @user_3 = FactoryGirl.create(:user)
+        @user_4 = FactoryGirl.create(:user)
+        @hand.users << @user_3
+        @hand.users << @user_4
+        hu = @hand.hand_users[0]
+        hu.hand_total_chip = 200
+        hu.save!
+        hu = @hand.hand_users[1]
+        hu.hand_total_chip = 100
+        hu.save!
+        hu = @hand.hand_users[2]
+        hu.hand_total_chip = 101
+        hu.last_action = TernActionFold.new
+        hu.save!
+        hu = @hand.hand_users[3]
+        hu.hand_total_chip = 200
+        hu.save!
+      end
+      it '100*4=400(全員), 1*3=3(user_1, 3, 4), 99*2=198のポット(user_1, 4)が作られる' do
+        pots = @ds.do!
+        expect(pots.size).to eq(3)
+        pot = pots[0]
+        expect(pot.chip).to eq(400)
+        expect(pot.hand_users.size).to eq(3)
+        user_ids = pot.hand_users.map{|p| p.user_id}
+        expect(user_ids.include?(@user_1.id)).to eq(true)
+        expect(user_ids.include?(@user_2.id)).to eq(true)
+        expect(user_ids.include?(@user_3.id)).to eq(false)
+        expect(user_ids.include?(@user_4.id)).to eq(true)
+        pot = pots[1]
+        expect(pot.chip).to eq(3)
+        expect(pot.hand_users.size).to eq(2)
+        user_ids = pot.hand_users.map{|p| p.user_id}
+        expect(user_ids.include?(@user_1.id)).to eq(true)
+        expect(user_ids.include?(@user_2.id)).to eq(false)
+        expect(user_ids.include?(@user_3.id)).to eq(false)
+        expect(user_ids.include?(@user_4.id)).to eq(true)
+        pot = pots[2]
+        expect(pot.chip).to eq(198)
+        expect(pot.hand_users.size).to eq(2)
+        user_ids = pot.hand_users.map{|p| p.user_id}
+        expect(user_ids.include?(@user_1.id)).to eq(true)
+        expect(user_ids.include?(@user_2.id)).to eq(false)
+        expect(user_ids.include?(@user_3.id)).to eq(false)
+        expect(user_ids.include?(@user_4.id)).to eq(true)
+      end
+    end
   end
-  describe 'get_min_hand_total_chip' do
+  describe '_get_min_hand_total_chip' do
     context 'user_1は100, user_2は200のとき' do
       before do
         hu = @hand.hand_users[0]
@@ -84,9 +133,32 @@ RSpec.describe DlBuildPotService, type: :service do
         hu.hand_total_chip = 200
         hu.save!
       end
-      it '100が返る' do
-        min_chip = @ds.send(:get_min_hand_total_chip, @hand)
-        expect(min_chip).to eq(100)
+      context 'before_min_chipは0のとき' do
+        before do
+          @before_min_chip = 0
+        end
+        it '100が返る' do
+          min_chip = @ds.send(:_get_min_hand_total_chip, @hand, @before_min_chip)
+          expect(min_chip).to eq(100)
+        end
+      end
+      context 'before_min_chipは100のとき' do
+        before do
+          @before_min_chip = 100
+        end
+        it '200が返る' do
+          min_chip = @ds.send(:_get_min_hand_total_chip, @hand, @before_min_chip)
+          expect(min_chip).to eq(200)
+        end
+      end
+      context 'before_min_chipは200のとき' do
+        before do
+          @before_min_chip = 200
+        end
+        it 'nilが返る' do
+          min_chip = @ds.send(:_get_min_hand_total_chip, @hand, @before_min_chip)
+          expect(min_chip).to eq(nil)
+        end
       end
     end
   end
